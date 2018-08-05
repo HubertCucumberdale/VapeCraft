8268ae53750e6f3135410b22ad0b7acca4456881
Hubert Cucumberdale <slaughterwg@gmail.com>
2018-08-05 12:35:08 -0400

    Initial commit with Forge configured and Minecraft running with an example mod that logs the registry name of a dirt block.

---
+++ src/main/java/com/cucumberkid9/vapecraft/VapeCraft.java
@@ -0,0 +1,31 @@
+package com.cucumberkid9.vapecraft;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = VapeCraft.MODID, name = VapeCraft.NAME, version = VapeCraft.VERSION)
+public class VapeCraft
+{
+    public static final String MODID = "vapecraft";
+    public static final String NAME = "VapeCraft";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
